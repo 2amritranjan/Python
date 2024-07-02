5f5dea0bae36e7676405667047bd4414bcd2bad7
Amrit Ranjan <2amritranjan@gmail.com>
2024-07-02 11:10:26 +0530

    container config done

--- string manipulation.py
+++
@@ -1,1 +0,0 @@
-print("first program- Python print function \n It is declared like this \n Print('What to print')")
---
+++ string_manipulation.py
@@ -0,0 +1,1 @@
+print("first program- Python print function \n It is declared like this \n Print('What to print')")
